7f814421ad85b46292ab5cb206ce561fafabbd92
Jung-Haechan <52622156+Jung-Haechan@users.noreply.github.com>
2020-01-25 00:53:15 +0900

    Revert "jquery 없이 nav 디테일 동적 생성"
    This reverts commit 99e9a5108ee2b9232effc108a42141d7fead89ed.

--- script.js
+++ script.js
@@ -1,43 +1,18 @@
-var navDetail = {
-  navList: document.getElementsByTagName('nav')[0].getElementsByTagName('a'),
-  contents: new Array(
-    '자신이 작곡한 MR 및 멜로디를 공유하세요!',
-    '자신의 감정을 담은 가사를 공유하세요!',
-    '자신의 목소리와 악기 실력을 프로듀서에게 어필하세요!',
-    '음악에 대한 다양한 의견을 공유하세요!',
-    '협업을 통해 완성한 여러분의 음악을 들려주세요!',
-  ),
-  hover: function(eventTarget) {
-    eventTarget.addEventListener('mouseover', function() {
-      eventTarget.setAttribute('style', 'color: pink');
+var nav = function(i, text) {
+  $('nav ul li:eq('+i+')').hover ( function(){
+    $(this).append('<div class="nav_detail">'+text+'</div>');
+    $(this).find('a').css('color', 'pink');
+  }, function(){
+    $('.nav_detail').fadeOut(300, function(){
+      $(this).children('li').remove();
     });
-    eventTarget.addEventListener('mouseout', function() {
-      eventTarget.removeAttribute('style');
-    });
-  },
-  show_del: function(eventTarget, detail, id){
-    eventTarget.addEventListener('mouseover', function() {
-      var newEl = document.createElement('div');
-      newEl.id = id;
-      newEl.innerHTML = detail;
-      eventTarget.parentNode.appendChild(newEl);
-    });
-    eventTarget.addEventListener('mouseout', function() {
-      eventTarget.parentNode.removeChild(document.getElementById(id));
-    });
-  },
-  nav_hover: function() {
-    for (i=0; i<this.navList.length; i++){
-      this.hover(this.navList[i]);
-      var index = (getIndex(this.navList[i].parentNode)-1)/2;
-      this.show_del(this.navList[i], this.contents[index], 'nav_det');
-    }
-  },
-  name_hover: function() {
-    var user_name = document.getElementById('name').getElementsByTagName('a')[0];
-    this.hover(user_name);
-    this.show_del(user_name, '마이페이지', 'mypage');
-  }
+    $(this).find('a').removeAttr('style');
+  });
 }
-navDetail.nav_hover();
-navDetail.name_hover();
+
+
+nav(0, '자신이 작곡한 MR 및 멜로디를 공유하세요!');
+nav(1, '자신의 감정을 담은 가사를 공유하세요!');
+nav(2, '자신의 목소리와 악기 실력을 프로듀서에게 어필하세요!');
+nav(3, '음악에 대한 다양한 의견을 공유하세요!');
+nav(4, '협업을 통해 완성한 여러분의 음악을 들려주세요!')
