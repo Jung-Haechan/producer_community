946afa92dc1dd429c64d7fa69d0e7f1b5316d3ce
Jung-Haechan <52622156+Jung-Haechan@users.noreply.github.com>
2020-02-03 01:20:11 +0900

    커뮤니티 소개

---
+++ hall_of_fame.php
@@ -0,0 +1,38 @@
+<?php
+  include 'headline.php';
+ ?>
+
+
+      <section>
+
+        <aside>
+          <div class="ad">
+            AD
+          </div>
+        </aside>
+
+        <article>
+
+          <div class="hall_of_fame">
+            <h2><a href="hall_of_fame.php">준비중입니다.</a></h2><br><br>
+
+          </div>
+        </article>
+
+        <aside>
+          <div class="ad">
+            AD
+          </div>
+        </aside>
+
+      </section>
+
+      <footer>
+        커뮤니티 주소
+      </footer>
+    </div>
+
+    <script src="script.js"></script>
+
+  </body>
+</html>
---
+++ q_and_a.php
@@ -0,0 +1,38 @@
+<?php
+  include 'headline.php';
+ ?>
+
+
+      <section>
+
+        <aside>
+          <div class="ad">
+            AD
+          </div>
+        </aside>
+
+        <article>
+
+          <div class="q_and_a">
+            <h2><a href="q_and_a.php">준비중입니다.</a></h2><br><br>
+
+          </div>
+        </article>
+
+        <aside>
+          <div class="ad">
+            AD
+          </div>
+        </aside>
+
+      </section>
+
+      <footer>
+        커뮤니티 주소
+      </footer>
+    </div>
+
+    <script src="script.js"></script>
+
+  </body>
+</html>
